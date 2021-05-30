2558935a43171a735b06fe48511fe20f3213f628
astr1k <astr1kyv@gmail.com>
2021-05-30 16:52:34 +0300

    test

---
+++ test.js
@@ -0,0 +1,15 @@
+var images = [
+  'images/1.png',
+  'images/2.png',
+  'images/3.png',
+  'images/4.png'
+];
+
+var img = document.getElementById("img");
+
+function imgShow(num) {
+	var num = Math.floor(Math.random() * 6);
+	img.style.backgroundImage = 'url("' + images[num] +'")';
+	img.style.backgroundRepeat = "no-repeat";
+}
+imgShow();
